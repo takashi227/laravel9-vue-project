aa41c3951f776b6e89d4be10239660db7e35072a
takashi <pero857942632@gmail.com>
2023-07-09 14:44:41 +0900

    Linkコンポーネントを作成

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
